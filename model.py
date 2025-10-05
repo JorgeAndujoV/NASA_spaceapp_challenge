981000a7ed84fef06e58b29bd1eb5813bb66362f
Jorge_Andujo <97417560+JorgeAndujoV@users.noreply.github.com>
2025-10-05 20:37:03 +0000

    intento1

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ def _load_model_once():
     
     try:
         # Ruta específica de tu modelo
-        MODEL_DIR = 'shark_model_optimized_v3.0.0_20251005_192236'
+        MODEL_DIR = 'shark_model_optimized_v3.0.0_20251005_203403'
         
         print("Cargando modelo de predicción de tiburones...")
         
